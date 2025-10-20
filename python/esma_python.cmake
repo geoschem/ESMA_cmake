dbb45bb31e137388af85e7057b9828668dc318c4
Lizzie Lundgren <elundgren@seas.harvard.edu>
2025-10-20 12:49:39 -0400

    Exclude python builds from GCHP build to avoid cmake warnings
    Signed-off-by: Lizzie Lundgren <elundgren@seas.harvard.edu>

--- python/esma_python.cmake
+++ python/esma_python.cmake
@@ -1,12 +1,14 @@
 # CMake code involving Python
 
-list (APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/f2py2")
-include (esma_find_python2_module)
-include (esma_check_python2_module)
-include (esma_add_f2py2_module)
-list (APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/f2py3")
-include (esma_find_python3_module)
-include (esma_check_python3_module)
-include (esma_add_f2py3_module)
+# Exclude the following from GCHP
+#list (APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/f2py2")
+#include (esma_find_python2_module)
+#include (esma_check_python2_module)
+#include (esma_add_f2py2_module)
+#list (APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/f2py3")
+#include (esma_find_python3_module)
+#include (esma_check_python3_module)
+#include (esma_add_f2py3_module)
 
-option(USE_F2PY "Turn on F2PY builds" ON)
+# Turn off F2PY builds in GCHP
+option(USE_F2PY "Turn on F2PY builds" OFF)
